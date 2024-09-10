c373aecb07e53b82e63a8f4c878778addd79b1b1
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-10 16:27:22 -0400

    feat: add create question method

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,7 @@
 import { type EntityID } from '@/core/entities/value-objects/entity-id'
 import { type Slug } from './value-objects/slug'
+import { type Optional } from '@/core/types/optional'
+
 import { Entity } from '@/core/entities/entity'
 
 type QuestionProps = {
@@ -13,4 +15,22 @@ type QuestionProps = {
   authorId: EntityID
 }
 
-export class Question extends Entity<QuestionProps> {}
+/**
+ * Create a type with 'createdAt' as optional. This is useful when creating a
+ * new entity, because the date can be provided or not.
+ */
+type CreateQuestionArgs = Optional<QuestionProps, 'createdAt'>
+
+export class Question extends Entity<QuestionProps> {
+  static create(props: CreateQuestionArgs, id?: EntityID) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return question
+  }
+}
