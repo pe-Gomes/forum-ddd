443007af6b13080bdcf7deb9507e2d756662dd5f
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-10 16:27:06 -0400

    feat: add create asnwer method

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,7 @@
-import { Entity } from '@/core/entities/entity'
 import { type EntityID } from '@/core/entities/value-objects/entity-id'
+import { type Optional } from '@/core/types/optional'
+
+import { Entity } from '@/core/entities/entity'
 
 type AnswerProps = {
   content: string
@@ -10,8 +12,22 @@ type AnswerProps = {
   questionId: EntityID
 }
 
+type CreateAnswerArgs = Optional<AnswerProps, 'createdAt'>
+
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
+  }
+
+  static create(props: CreateAnswerArgs, id?: EntityID) {
+    const question = new Answer(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return question
   }
 }
