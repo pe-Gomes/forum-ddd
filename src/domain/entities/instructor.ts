2589b98c16092e172b1bea0f584d7aff39599e86
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-10 16:27:49 -0400

    feat: add create instructor method

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,7 +1,13 @@
+import { type EntityID } from '@/core/entities/value-objects/entity-id'
+
 import { Entity } from '@/core/entities/entity'
 
 type InstructorProps = {
   name: string
 }
 
-export class Instructor extends Entity<InstructorProps> {}
+export class Instructor extends Entity<InstructorProps> {
+  create(args: InstructorProps, id?: EntityID) {
+    return new Instructor(args, id)
+  }
+}
