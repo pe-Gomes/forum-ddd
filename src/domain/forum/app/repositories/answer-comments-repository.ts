d89d60ac33c4ba9def00dab4b0d706f7671cbe2f
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-12 09:28:56 -0400

    feat:add comment on answer use case & repo

---
+++ src/domain/forum/app/repositories/answer-comments-repository.ts
@@ -0,0 +1,7 @@
+import { type AnswerComment } from '../../enterprise/entities/answer-comment'
+
+export interface AnswerCommentsRepository {
+  create(comment: AnswerComment): Promise<void>
+
+  getById(id: string): Promise<AnswerComment | null>
+}
---
+++ tests/in-memory-repository/answer-comment.ts
@@ -0,0 +1,18 @@
+import { type AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { type AnswerCommentsRepository } from '@/domain/forum/app/repositories/answer-comments-repository'
+
+export class InMemoryAnswerCommentRepository
+  implements AnswerCommentsRepository
+{
+  public comments: AnswerComment[] = []
+
+  async create(comment: AnswerComment) {
+    await Promise.resolve(this.comments.push(comment))
+  }
+
+  async getById(id: string) {
+    return await Promise.resolve(
+      this.comments.find((comment) => comment.id.toString() === id) ?? null,
+    )
+  }
+}
