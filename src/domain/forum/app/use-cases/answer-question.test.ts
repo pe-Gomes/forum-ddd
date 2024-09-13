be74650f4df11fada05f937aa2f9f6fe3d7464be
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-13 09:57:29 -0400

    feat:handle attachments on answer question usecase

--- src/domain/forum/app/use-cases/answer-question.test.ts
+++ src/domain/forum/app/use-cases/answer-question.test.ts
@@ -1,6 +1,7 @@
 import { expect, it, describe, beforeEach } from 'vitest'
 import { InMemoryAnswerRepository } from '@tests/in-memory-repository/answer'
 import { AnswerQuestionUseCase } from './answer-question'
+import { EntityID } from '@/core/entities/value-objects/entity-id'
 
 let repository: InMemoryAnswerRepository
 let useCase: AnswerQuestionUseCase
@@ -24,7 +25,11 @@ describe('Answer Question Use Case', () => {
     expect(res.value?.answer.authorId.toString()).toEqual('instructorId')
     expect(res.value?.answer.questionId.toString()).toEqual('questionId')
     expect(res.value?.answer.createdAt).toBeDefined()
-    expect(res.value?.answer.attachments).toBeDefined()
+    expect(res.value?.answer.attachments.currentItems).toHaveLength(2)
+    expect(res.value?.answer.attachments.currentItems).toEqual([
+      expect.objectContaining({ attachmentId: new EntityID('1') }),
+      expect.objectContaining({ attachmentId: new EntityID('2') }),
+    ])
 
     expect(repository.answers[0].id).toBe(res.value?.answer.id)
   })
--- src/domain/forum/app/use-cases/answer-question.ts
+++ src/domain/forum/app/use-cases/answer-question.ts
@@ -2,6 +2,8 @@ import { EntityID } from '@/core/entities/value-objects/entity-id'
 import { Answer } from '../../enterprise/entities/answer'
 import { type AnswersRepository } from '../repositories/answers-repository'
 import { success, type Either } from '@/core/either'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 type AnswerQuestionRequest = {
   instructorId: string
@@ -19,12 +21,24 @@ export class AnswerQuestionUseCase {
     instructorId,
     questionId,
     content,
+    attachmentsIds,
   }: AnswerQuestionRequest): Promise<AnswerQuestionResponse> {
     const answer = Answer.create({
       content,
       authorId: new EntityID(instructorId),
       questionId: new EntityID(questionId),
     })
+
+    if (attachmentsIds) {
+      const questionAttachments = attachmentsIds.map((attachmentId) => {
+        return AnswerAttachment.create({
+          attachmentId: new EntityID(attachmentId),
+          answerId: answer.id,
+        })
+      })
+
+      answer.attachments = new AnswerAttachmentList(questionAttachments)
+    }
 
     await this.answersRepository.create(answer)
 
