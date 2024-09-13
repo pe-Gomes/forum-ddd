ada5036acab1c909cb336692cc8834c3e9a6fc95
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-13 10:31:03 -0400

    feat:handle answer attachments on delete answer

--- src/domain/forum/app/use-cases/delete-answer.test.ts
+++ src/domain/forum/app/use-cases/delete-answer.test.ts
@@ -5,31 +5,37 @@ import { EntityID } from '@/core/entities/value-objects/entity-id'
 import { type AnswersRepository } from '../repositories/answers-repository'
 import { createAnswer } from '@tests/factory/answer'
 import { NotAllowedError, ResourceNotFoundError } from '@/core/errors'
+import { InMemoryAnswerAttachmentRepository } from '@tests/in-memory-repository/answer-attachment'
+import { createAnswerAttachment } from '@tests/factory/answer-attachment'
 
 let answerRepository: AnswersRepository
+let answerAttachmentRepo: InMemoryAnswerAttachmentRepository
 let sut: DeleteAnswerUseCase
 
 describe('DeleteAnswer Use Case', () => {
   beforeEach(() => {
     answerRepository = new InMemoryAnswerRepository()
-    sut = new DeleteAnswerUseCase(answerRepository)
+    answerAttachmentRepo = new InMemoryAnswerAttachmentRepository()
+    sut = new DeleteAnswerUseCase(answerRepository, answerAttachmentRepo)
   })
 
   it('should delete a question an existing question', async () => {
-    await answerRepository.create(
-      createAnswer(
-        {
-          authorId: new EntityID('2'),
-        },
-        new EntityID('1'),
-      ),
+    const answer = createAnswer({
+      authorId: new EntityID('2'),
+    })
+
+    await answerRepository.create(answer)
+    answerAttachmentRepo.answerAttachments.push(
+      createAnswerAttachment({ answerId: answer.id }),
+      createAnswerAttachment({ answerId: answer.id }),
     )
 
-    await sut.execute({ id: '1', authorId: '2' })
+    await sut.execute({ id: answer.id.toString(), authorId: '2' })
 
-    const shouldBeNull = await answerRepository.getById('1')
+    const shouldBeNull = await answerRepository.getById(answer.id.toString())
 
     expect(shouldBeNull).toBeNull()
+    expect(answerAttachmentRepo.answerAttachments).toHaveLength(0)
   })
 
   it('should throw an error if question does not exist', async () => {
--- src/domain/forum/app/use-cases/delete-answer.ts
+++ src/domain/forum/app/use-cases/delete-answer.ts
@@ -1,6 +1,7 @@
 import { type Either, failure, success } from '@/core/either'
 import { type AnswersRepository } from '../repositories/answers-repository'
 import { NotAllowedError, ResourceNotFoundError } from '@/core/errors'
+import { type AnswerAttachmentsRepository } from '../repositories/answer-attachments-repository'
 
 type DeleteAnswerRequest = {
   id: string
@@ -13,13 +14,16 @@ type DeleteAnswerResponse = Either<
 >
 
 export class DeleteAnswerUseCase {
-  constructor(private questionRepository: AnswersRepository) {}
+  constructor(
+    private answerRepository: AnswersRepository,
+    private answerAttachmentRepository: AnswerAttachmentsRepository,
+  ) {}
 
   async execute({
     id,
     authorId,
   }: DeleteAnswerRequest): Promise<DeleteAnswerResponse> {
-    const question = await this.questionRepository.getById(id)
+    const question = await this.answerRepository.getById(id)
 
     if (!question) {
       return failure(new ResourceNotFoundError())
@@ -29,7 +33,8 @@ export class DeleteAnswerUseCase {
       return failure(new NotAllowedError())
     }
 
-    await this.questionRepository.delete(id)
+    await this.answerRepository.delete(id)
+    await this.answerAttachmentRepository.deleteManyByAnswerId(id)
 
     return success({})
   }
